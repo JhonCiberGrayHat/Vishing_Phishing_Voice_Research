723dd719c9cb7aafb0ed3e348410c4c16577edad
‘JhonCiberGrayHat’ <jhon.camacho6031@gmail.com>
2026-04-28 18:22:29 -0500

    Se complementa el codigo de estadistica descritiva para la data de delitos informaticos

--- descriptivas.py
+++ descriptivas.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import seaborn as sns
 
 # URL RAW del archivo CSV en GitHub
 #url = "https://raw.githubusercontent.com/usuario/repositorio/main/datos/archivo.csv"
@@ -7,5 +9,8 @@ import pandas as pd
 df = pd.read_csv("Conteo_de_Víctimas_V2_20251211_delitos_informaticos.csv")
 
 # Verificar carga
-print(df.head())
-print(df.info())
+print(df.head())  # conozco los encabezados de la data
+print(df.info())  # conozco los generales de la data
+
+print(df.describe()) # medidas descriptivas univariadas 
+
